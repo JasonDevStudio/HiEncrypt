0b2bd47c596a0a5be6911d569b5475e3ee2c78f9
Jason Yao <jiedev@163.com>
2023-09-24 07:12:39 +0800

    加密工具

---
+++ HiEncrypt/EncryptConf.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiCmd;
+/// <summary>
+/// 根配置实体
+/// </summary>
+internal class EncryptConf
+{
+    /// <summary>
+    /// 源目录
+    /// </summary>
+    public string SourceDir { get; set; }
+
+    /// <summary>
+    /// 目标目录
+    /// </summary>
+    public string TargetDir { get; set; }
+
+    /// <summary>
+    /// 加密程序
+    /// </summary>
+    public string EncryptionProgram { get; set; }
+
+    /// <summary>
+    /// 程序参数
+    /// </summary>
+    public string Arguments { get; set; }
+
+    /// <summary>
+    /// 模块列表
+    /// </summary>
+    public List<Module> Modules { get; set; }
+}
+
+/// <summary>
+/// 模块实体
+/// </summary>
+public class Module
+{
+    /// <summary>
+    /// 模块名称
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 程序集列表
+    /// </summary>
+    public List<AssemblyFile> Assemblies { get; set; }
+}
+
+/// <summary>
+/// 程序集实体
+/// </summary>
+public class AssemblyFile
+{ 
+    /// <summary>
+    /// 源文件
+    /// </summary>
+    public string Source { get; set; }
+
+    /// <summary>
+    /// 目标文件
+    /// </summary>
+    public string Target { get; set; }
+}
---
+++ HiEncrypt/Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using HiCmd;
+using McMaster.Extensions.CommandLineUtils;
+using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CommandLineApplication.Execute<Program>(args);
+    }
+
+    [Option(ShortName = "f", Description = "加密配置文件路径", LongName = "File")]
+    public string File { get; set; }
+
+    [Option(ShortName = "c", Description = "执行命令", LongName = "Cmd")]
+    public string Command { get; set; }
+
+    private void OnExecute()
+    {
+        switch (Command.ToLower())
+        {
+            case DicCmd.encrypt:
+                OnEncrypt();
+                break;
+        }
+    }
+
+    private void OnEncrypt()
+    {
+        var json = System.IO.File.ReadAllText(File);
+        var encryptor = JsonConvert.DeserializeObject<EncryptConf>(json);
+        var commands = new List<string>();
+
+        foreach (var module in encryptor.Modules)
+        {
+            foreach (var assembly in module.Assemblies)
+            {
+                commands.Add($"{encryptor.EncryptionProgram} {encryptor.Arguments} {assembly.Source} {assembly.Target}");
+            }
+        }
+
+        ExecuteMultipleCommands(commands);
+    }
+
+    /// <summary>
+    /// 使用标准输入流执行多行命令
+    /// </summary>
+    /// <param name="commands">命令集合</param>
+    static void ExecuteMultipleCommands(List<string> commands)
+    {
+        // 创建一个ProcessStartInfo对象
+        ProcessStartInfo psi = new ProcessStartInfo
+        {
+            FileName = "cmd.exe",
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
+
+        // 启动进程
+        using (Process process = new Process { StartInfo = psi })
+        {
+            process.OutputDataReceived += (s, e) => Console.WriteLine(e.Data);
+            process.ErrorDataReceived += (s, e) => Console.WriteLine(e.Data);
+            process.Start();
+            process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
+
+            // 获取标准输入流并写入多行命令
+            using (StreamWriter sw = process.StandardInput)
+            {
+                if (sw.BaseStream.CanWrite)
+                {
+                    foreach (var command in commands)
+                    {
+                        sw.WriteLine(command);
+                    }
+                }
+            }
+
+            // 等待进程执行完毕
+            process.WaitForExit();
+        }
+    }
+}
+
+static class DicCmd
+{
+    /// <summary>
+    /// 加密
+    /// </summary>
+    public const string encrypt = nameof(encrypt);
+}
